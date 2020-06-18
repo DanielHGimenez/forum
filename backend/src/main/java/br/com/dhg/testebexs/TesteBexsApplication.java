274eece1c208ed1db2bbb78bdd77ad8d4e0620b7
DanielHGimenez <danielhgimenez027@gmail.com>
2020-06-18 12:18:25 -0300

    subindo projeto base do backend

---
+++ backend/src/main/java/br/com/dhg/testebexs/TesteBexsApplication.java
@@ -0,0 +1,13 @@
+package br.com.dhg.testebexs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteBexsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteBexsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/dhg/testebexs/TesteBexsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dhg.testebexs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteBexsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
