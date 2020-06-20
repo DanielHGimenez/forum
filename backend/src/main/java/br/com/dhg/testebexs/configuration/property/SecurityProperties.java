d8601cff79f05ce36a7d6630fc9b3b4dd5fc63fd
DanielHGimenez <danielhgimenez027@gmail.com>
2020-06-20 14:55:39 -0300

    removendo @PropertySource

--- backend/src/main/java/br/com/dhg/testebexs/configuration/property/SecurityProperties.java
+++ backend/src/main/java/br/com/dhg/testebexs/configuration/property/SecurityProperties.java
@@ -2,14 +2,8 @@ package br.com.dhg.testebexs.configuration.property;
 
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 import org.springframework.stereotype.Component;
 
-@PropertySources({
-        @PropertySource("classpath:application.properties"),
-        @PropertySource("classpath:application-${spring.profiles.active}.properties")
-})
 @Component
 @Getter
 public class SecurityProperties {
