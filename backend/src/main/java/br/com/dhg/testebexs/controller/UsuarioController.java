745a45f95a142ebc961b51d3b6ddae4944ff1edb
DanielHGimenez <danielhgimenez027@gmail.com>
2020-06-20 16:07:11 -0300

    Alterando status code recebido quando um usuário é criado com sucesso

--- backend/src/main/java/br/com/dhg/testebexs/controller/UsuarioController.java
+++ backend/src/main/java/br/com/dhg/testebexs/controller/UsuarioController.java
@@ -3,9 +3,11 @@ package br.com.dhg.testebexs.controller;
 import br.com.dhg.testebexs.dto.CadastrarUsuarioDTO;
 import br.com.dhg.testebexs.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -18,6 +20,7 @@ public class UsuarioController {
     private UsuarioService service;
 
     @PostMapping("/cadastro")
+    @ResponseStatus(HttpStatus.CREATED)
     public void cadastrar(@Valid @RequestBody CadastrarUsuarioDTO cadastrarUsuarioDTO) {
 
         service.cadastrar(cadastrarUsuarioDTO.getNome(), cadastrarUsuarioDTO.getSenha());
