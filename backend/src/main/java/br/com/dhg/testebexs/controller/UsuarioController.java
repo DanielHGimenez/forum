03bec028feec66db5628cf718acf4b440eef43fb
DanielHGimenez <danielhgimenez027@gmail.com>
2020-06-21 00:26:20 -0300

    removendo /public do endereço de login e cadastro

--- backend/src/main/java/br/com/dhg/testebexs/controller/UsuarioController.java
+++ backend/src/main/java/br/com/dhg/testebexs/controller/UsuarioController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/public/autenticacao")
+@RequestMapping("/autenticacao")
 public class UsuarioController {
 
     @Autowired
