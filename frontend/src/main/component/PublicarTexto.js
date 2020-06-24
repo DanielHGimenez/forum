4b8ac54377169a8912dcccb720072c450ccc80ae
DanielHGimenez <danielhgimenez027@gmail.com>
2020-06-24 14:29:44 -0300

    Refatorando PublicarTexto para contemplar mais cenários

---
+++ frontend/src/main/component/PublicarTexto.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import ModalPublicarTexto from '../component/ModalPublicarTexto';
+import Button from './Button';
+import Api from '../service/Api';
+import '../style/PublicarTexto.css';
+
+
+export default function PublicarTexto({ store, onSubmit, labelModal }) {
+
+    const [ isAutenticado, setIsAutenticado ] = useState(false);
+    const [ isPublicarTexto, setIsPublicarTexto ] = useState(false);
+
+    useEffect(() => {
+        if (store.getState().credenciais != null) {
+            setIsAutenticado(true);
+        }
+    }, []);
+
+    store.subscribe(() => {
+        if (store.getState().credenciais != null) {
+            setIsAutenticado(true);
+        } else {
+            setIsAutenticado(false);
+        }
+    });
+
+    const publicarTexto = (texto) => {
+        setIsPublicarTexto(false);
+        onSubmit(texto);
+    };
+    
+    return (
+        <>
+            <ModalPublicarTexto
+                show={ isPublicarTexto }
+                onHide={ () => setIsPublicarTexto(false) }
+                onSubmit={ publicarTexto }
+                label={ labelModal }
+            />
+            {isAutenticado &&
+                <Button className="Publicar-texto" onClick={ () => setIsPublicarTexto(true) }>Publicar</Button>
+            }
+        </>
+    )
+}
