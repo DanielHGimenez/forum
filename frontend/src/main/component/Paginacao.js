7842f59a05c9a9feae98a3c50237c1a10702fb67
DanielHGimenez <danielhgimenez027@gmail.com>
2020-06-24 02:19:12 -0300

    implementação de componente de paginação

---
+++ frontend/src/main/component/Paginacao.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Pagination } from 'react-bootstrap';
+import '../style/Paginacao.css'
+
+export default function Paginacao({ paginaAtual, totalPaginas, onClick, className }) {
+    return (
+        <Pagination className={ "Paginacao " + className }>
+            {paginaAtual >= 3 && 
+            <>
+                <Pagination.Item onClick={ () => onClick(1) }>1</Pagination.Item>
+                <Pagination.Ellipsis />
+            </>
+            }
+            {paginaAtual > 1 &&
+                <Pagination.Item onClick={ () => onClick(paginaAtual - 1) }>{ paginaAtual - 1 }</Pagination.Item>
+            }
+            <Pagination.Item active>{ paginaAtual }</Pagination.Item>
+            {paginaAtual != totalPaginas && 
+                <Pagination.Item onClick={ () => onClick(paginaAtual + 1) }>{ paginaAtual + 1 }</Pagination.Item>
+            }
+            {paginaAtual < totalPaginas - 1 && 
+            <>
+                <Pagination.Ellipsis />
+                <Pagination.Item onClick={ () => onClick(totalPaginas) }>{ totalPaginas }</Pagination.Item>
+            </>
+            }
+        </Pagination>
+    )
+}
