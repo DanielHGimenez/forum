c1179ef75daf7fdbd499e45e3f1ab06d5935451e
DanielHGimenez <danielhgimenez027@gmail.com>
2020-06-24 22:10:56 -0300

    corrigindo paginação

--- frontend/src/main/component/Paginacao.js
+++ frontend/src/main/component/Paginacao.js
@@ -4,26 +4,32 @@ import '../style/Paginacao.css'
 
 export default function Paginacao({ paginaAtual, totalPaginas, onClick, className }) {
     return (
-        <Pagination className={ "Paginacao " + className }>
-            {paginaAtual >= 3 && 
-            <>
-                <Pagination.Item onClick={ () => onClick(1) }>1</Pagination.Item>
-                <Pagination.Ellipsis />
-            </>
-            }
-            {paginaAtual > 1 &&
-                <Pagination.Item onClick={ () => onClick(paginaAtual - 1) }>{ paginaAtual - 1 }</Pagination.Item>
-            }
-            <Pagination.Item active>{ paginaAtual }</Pagination.Item>
-            {paginaAtual !== totalPaginas && 
-                <Pagination.Item onClick={ () => onClick(paginaAtual + 1) }>{ paginaAtual + 1 }</Pagination.Item>
-            }
-            {paginaAtual <= totalPaginas - 2 &&
-            <>
-                <Pagination.Ellipsis />
-                <Pagination.Item onClick={ () => onClick(totalPaginas) }>{ totalPaginas }</Pagination.Item>
-            </>
-            }
-        </Pagination>
+        <>
+        {totalPaginas > 0 &&
+            <Pagination className={ "Paginacao " + className }>
+                {paginaAtual >= 3 && 
+                    <Pagination.Item onClick={ () => onClick(1) }>1</Pagination.Item>
+                }
+                {paginaAtual >= 4 &&
+                    <Pagination.Ellipsis />
+                }
+                {paginaAtual > 1 &&
+                    <Pagination.Item onClick={ () => onClick(paginaAtual - 1) }>{ paginaAtual - 1 }</Pagination.Item>
+                }
+                
+                <Pagination.Item active>{ paginaAtual }</Pagination.Item>
+                
+                {paginaAtual !== totalPaginas && 
+                    <Pagination.Item onClick={ () => onClick(paginaAtual + 1) }>{ paginaAtual + 1 }</Pagination.Item>
+                }
+                {paginaAtual < totalPaginas - 2 &&
+                    <Pagination.Ellipsis />
+                }
+                {paginaAtual <= totalPaginas - 2 &&
+                    <Pagination.Item onClick={ () => onClick(totalPaginas) }>{ totalPaginas }</Pagination.Item>
+                }
+            </Pagination>
+        }
+        </>
     )
 }
